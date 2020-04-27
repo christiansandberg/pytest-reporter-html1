5d51c8c931c71b652519742382f056c5bac272fe
Christian Sandberg <christiansandberg@me.com>
2020-04-27 17:48:23 +0200

    More design work

--- pytest_reporter_html1/plugin.py
+++ pytest_reporter_html1/plugin.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 import re
 from functools import partial
 import os.path
+from inspect import cleandoc
 
 import pytest
 
@@ -14,9 +15,22 @@ except ImportError:
 else:
     conv = Ansi2HTMLConverter(escaped=False)
 
+try:
+    from docutils.core import publish_parts
+except ImportError:
+    publish_parts = None
+
 
 def pytest_reporter_template_dir():
     return os.path.join(os.path.dirname(__file__), "templates")
+
+
+def rst2html(rst):
+    parts = publish_parts(
+        source=rst,
+        writer_name='html5',
+    )
+    return parts
 
 
 @pytest.hookimpl(tryfirst=True)
@@ -25,6 +39,8 @@ def pytest_reporter_modify_env(env):
     env.filters["strftime"] = lambda ts, fmt: datetime.fromtimestamp(ts).strftime(fmt)
     env.filters["timedelta"] = lambda ts: timedelta(seconds=ts)
     env.filters["ansi"] = partial(conv.convert, full=False) if conv else str
+    env.filters["cleandoc"] = cleandoc
+    env.filters["rst2html"] = rst2html
     env.filters["css_minify"] = partial(re.sub, r"\s+", " ")
     env.filters["html_minify"] = partial(re.sub, r">\s+<", "> <")
 
