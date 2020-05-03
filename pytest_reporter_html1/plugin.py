0af6d0f2d74bae2191e64972c14c1b861a99a83b
Christian Sandberg <christiansandberg@me.com>
2020-05-03 21:15:52 +0200

    Let the template handle the engine

--- pytest_reporter_html1/plugin.py
+++ pytest_reporter_html1/plugin.py
@@ -1,66 +1,60 @@
+from pathlib import Path
 from datetime import datetime, timedelta
 import re
-from functools import partial
-import os.path
-from pathlib import Path
 from inspect import cleandoc
 from base64 import b64encode
 
-import pytest
-try:
-    from ansi2html import Ansi2HTMLConverter
-    from ansi2html.style import get_styles
-except ImportError:
-    HAS_ANSI = False
-else:
-    HAS_ANSI = True
-    conv = Ansi2HTMLConverter(escaped=False)
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+from ansi2html import Ansi2HTMLConverter
+from ansi2html.style import get_styles
 from docutils.core import publish_parts
 import htmlmin
 
 
-ICONS_PATH = Path(__file__).parent / "templates" / "html1" / "icons"
+TEMPLATE_PATH = Path(__file__).parent / "templates"
+ICONS_PATH = TEMPLATE_PATH / "html1" / "icons"
 
 
-def pytest_reporter_template_dir():
-    return os.path.join(os.path.dirname(__file__), "templates")
+def pytest_reporter_context(context, config):
+    # category/style: background-color, color
+    context.setdefault("colors", {
+        "passed": ("#43A047", "#FFFFFF"),
+        "failed": ("#F44336", "#FFFFFF"),
+        "error": ("#B71C1C", "#FFFFFF"),
+        "xfailed": ("#EF9A9A", "#333333"),
+        "xpassed": ("#A5D6A7", "#333333"),
+        "skipped": ("#9E9E9E", "#FFFFFF"),
+        "warning": ("#FBC02D", "#333333"),
+        "green": ("#43A047", "#FFFFFF"),
+        "red": ("#E53935", "#FFFFFF"),
+        "yellow": ("#FBC02D", "#333333"),
+    })
 
-
-def rst2html(rst):
-    if publish_parts is not None:
-        parts = publish_parts(source=rst, writer_name="html5")
-        return parts["body"]
-    else:
-        return re.sub(r"\n", "<br>", rst)
-
-
-@pytest.hookimpl(tryfirst=True)
-def pytest_reporter_modify_env(env):
+def pytest_reporter_render(template_name, dirs, context):
+    conv = Ansi2HTMLConverter(escaped=False)
+    env = Environment(
+        loader=FileSystemLoader(dirs + [str(TEMPLATE_PATH)]),
+        autoescape=select_autoescape(["html", "htm", "xml"]),
+    )
+    env.globals["icons"] = {
+        icon.stem: (
+            "data:image/svg+xml;base64," +
+            b64encode(icon.read_bytes()).decode("utf-8")
+        ) for icon in ICONS_PATH.glob("*.svg")
+    }
+    env.globals["get_ansi_styles"] = get_styles
     env.filters["repr"] = repr
     env.filters["strftime"] = lambda ts, fmt: datetime.fromtimestamp(ts).strftime(fmt)
     env.filters["timedelta"] = lambda ts: timedelta(seconds=ts)
-    env.filters["ansi"] = lambda s: conv.convert(s, full=False) if HAS_ANSI else s
+    env.filters["ansi"] = lambda s: conv.convert(s, full=False)
     env.filters["cleandoc"] = cleandoc
-    env.filters["rst"] = rst2html
-    env.filters["base64"] = lambda s: b64encode(b).decode("utf-8")
+    env.filters["rst"] = lambda s: publish_parts(source=s, writer_name="html5")["body"]
     env.filters["css_minify"] = lambda s: re.sub(r"\s+", " ", s)
 
-
-def pytest_reporter_context(context):
-    if HAS_ANSI:
-        context["get_ansi_styles"] = get_styles
-    icons = context.setdefault("icons", {})
-    for icon in ICONS_PATH.glob("*.svg"):
-        icons[icon.stem] = (
-            "data:image/svg+xml;base64," +
-            b64encode(icon.read_bytes()).decode("utf-8")
-        )
-
-
-@pytest.hookimpl(hookwrapper=True)
-def pytest_reporter_render():
-    outcome = yield
-    html = outcome.get_result()
-    if htmlmin is not None:
-        minified = htmlmin.minify(html, remove_comments=True)
-        outcome.force_result(minified)
+    try:
+        template = env.get_template(template_name)
+    except TemplateNotFound:
+        return
+    html = template.render(context)
+    minified = htmlmin.minify(html, remove_comments=True)
+    return minified
