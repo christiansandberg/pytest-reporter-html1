f6a38c290cdab71f489154d29973e8224f5f35e2
Christian Sandberg <christian.sandberg@xylem.com>
2024-02-28 20:26:21 +0100

    Remove usage of pkg_resources

--- pytest_reporter_html1/__init__.py
+++ pytest_reporter_html1/__init__.py
@@ -1,9 +1,8 @@
-from pkg_resources import get_distribution, DistributionNotFound
-
+from importlib.metadata import version
 
 try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
+    __version__ = version("pytest-reporter-html1")
+except Exception:
     # package is not installed
     __version__ = "unknown"
 
